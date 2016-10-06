75c5809bd25426acd56177df3664eed120e86e2b
Ruben Oostinga <roostinga@xebia.com>
2016-10-06 14:01:25 +0200

    TitleElement component, some notes

--- presentation/index.js
+++ presentation/index.js
@@ -46,125 +46,76 @@ const images = {
 
 preloader(images);
 
-const theme = createTheme({
-  primary: "#ff4081"
-});
-
-export default class Presentation extends React.Component {
-  render() {
-    return (
-      <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <AsciinemaPlayer src="assets/jest-nocache.json" autoplay />
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
-          </Slide>
-        </Deck>
-      </Spectacle>
-    );
-  }
+function TitleElement({ children }) {
+  const style = {
+    flex: '0 0 auto',
+    justifyContent: 'space-between'
+  };
+  return (
+    <Fill {...{ style }}>
+    {children}
+    </Fill>
+  );
 }
+
+export default () => (
+  <Spectacle>
+    <Deck>
+      <Slide>
+        <Heading size={1} fit>
+          Snapshot tests
+        </Heading>
+        <Heading size={2} fit>
+          with Mocha in Jest
+        </Heading>
+        <Layout>
+        <TitleElement>Ruben Oostinga</TitleElement>
+        <TitleElement>Xebia</TitleElement>
+        <TitleElement>@RubenOostinga</TitleElement>
+        <TitleElement>https://github.com/0xR</TitleElement>
+        </Layout>
+      </Slide>
+      <Slide>
+        <Heading size={3} fit>
+          About this talk
+        </Heading>
+      </Slide>
+      <Slide>
+        <Heading size={3} fit>
+          What is snapshot testing
+        </Heading>
+      </Slide>
+      <Slide>
+        <Heading size={4} fit>
+          What is snapshot testing
+        </Heading>
+        <List>
+          <ListItem>asdf</ListItem>
+        </List>
+      </Slide>
+      <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
+        <CodePane
+          lang="jsx"
+          source={require("raw!../assets/deck.example")}
+          margin="20px auto"
+        />
+      </Slide>
+    </Deck>
+  </Spectacle>
+);
+
+/* what is snapshot testing
+ * what problem does it solve
+ * Conferencetalk Driven development, not gonna fall for free tool ;)
+ * Christoph worked on it for over a year, some months
+ * chai-jest-snapshot
+ * disadvantage: no way to hook into the CLI, doesn't clean up snapshots
+ * migrating:
+ * combining chai / jest
+ * inject loader compatibility layer
+ * proptype warnings because of resetModules
+ *
+ * disadvantage: slow for integration, increade default timeout, cache sometimes confusing
+ * only error: user error
+ * pros: nice console output, DX: console.log, watch, rerun failed tests first, nice diff when test fails, buffered console, mocks that work, coverage of not included files
+ */
